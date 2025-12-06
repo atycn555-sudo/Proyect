5223be3704710caffdc1211ab47502b3bd1dd218
atycn555-sudo <atycn555@gmail.com>
2025-12-05 19:52:37 -0600

    Update VeterinarianController.java

--- src/main/java/com/example/vet/controller/VeterinarianController.java
+++ src/main/java/com/example/vet/controller/VeterinarianController.java
@@ -6,7 +6,7 @@ import com.example.vet.model.Veterinarian;
 import com.example.vet.service.VeterinarianService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid; // <-- 1. IMPORT THIS
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
